7efc60ea25ea0711238f0a94681fd0df1c3cf0f3
bsrecker <42258510+bsrecker@users.noreply.github.com>
2022-09-03 22:30:48 -0500

    Updated solution.

--- Day 1.py
+++ Day 1.py
@@ -3,16 +3,16 @@ measurements = sonar_report.readlines()
 
 increased_count = 0
 
-for index, value in enumerate(measurements):
-    current_depth = int(value.strip())
-    previous_depth = int(measurements[index - 1].strip())
+for i in range(len(measurements)):
+    current_depth = int(measurements[i].strip())
+    previous_depth = int(measurements[i - 1].strip())
 
     if current_depth > previous_depth:
         increased_count += 1
         print(f'{current_depth} (increased)')
 
     else:
-        print(f'{current_depth} (N/A)' if index == 0 else f"{current_depth} (decreased)")
+        print(f'{current_depth} (N/A)' if i == 0 else f"{current_depth} (decreased)")
 
 
 print(f'Number of increases: {increased_count}')
